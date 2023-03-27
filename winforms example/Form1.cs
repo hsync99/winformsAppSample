b9708b2fe47cfc9fbe7f27c02845f8ca2008b46a
Leonid Gavrilov <hsync99@gmail.com>
2023-03-27 14:58:47 +0600

    Добавьте файлы проекта.

---
+++ winforms example/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winforms_example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                listView1.Items.Add("text" + i.ToString());
+            }
+            
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>(); 
+            foreach(var i in listView1.Items) 
+            {
+                list.Add(i.ToString());
+            }
+            Form2 f = new Form2(list);
+            f.ShowDialog(); 
+           
+           
+        }
+    }
+}
---
+++ winforms example/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winforms_example
+{
+    public partial class Form2 : Form
+    {
+     
+        public Form2(List<string> items)
+        {
+            InitializeComponent();
+            foreach (string item in items) { 
+                listView1.Items.Add(item);
+            }
+        }
+        public class DataFromForm1
+        {
+            List<string> data = new List<string>();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
