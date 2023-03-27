e9c1860e4c7be4f2c34abfc4c4019c98f379b6af
Leonid Gavrilov <hsync99@gmail.com>
2023-03-27 15:08:53 +0600

    step

--- winforms example/Form2.cs
+++ winforms example/Form2.cs
@@ -20,11 +20,7 @@ namespace winforms_example
                 listView1.Items.Add(item);
             }
         }
-        public class DataFromForm1
-        {
-            List<string> data = new List<string>();
-
-        }
+      
         private void button1_Click(object sender, EventArgs e)
         {
            
